d84a18dbac9224ab0918f759bc0c6b2a2a2c993a
Thiago Goulart <thiagoalgo@gmail.com>
2024-09-13 23:47:47 -0300

    wip

--- functions/index.js
+++ functions/index.js
@@ -10,14 +10,6 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 exports.createCheckout = onRequest((request, response) => {
   if (request.method !== 'POST') {
     response.sendStatus(405)
@@ -27,3 +19,47 @@ exports.createCheckout = onRequest((request, response) => {
   const data = request.body
   response.json(data)
 });
+
+
+// const axios = require('axios');
+
+// const options = {
+//   method: 'POST',
+//   url: 'https://sandbox.api.pagseguro.com/checkouts',
+//   headers: {
+//     accept: '*/*',
+//     Authorization: 'Bearer <token>',
+//     'Content-type': 'application/json'
+//   },
+//   data: {
+//     customer: {
+//       phone: {country: '55', area: '48', number: '998421117'},
+//       Name: 'Thiago Alves Goulart',
+//       email: 'thiagoalgo@gmail.com',
+//       tax_id: '04076073908'
+//     },
+//     reference_id: 'order-id-123456',
+//     customer_modifiable: true,
+//     items: [{reference_id: 'product-id-0123'}],
+//     soft_descriptor: 'Standard',
+//     additional_amount: 0,
+//     discount_amount: 0,
+//     payment_methods: [{type: 'CREDIT_CARD'}, {type: 'DEBIT_CARD'}, {type: 'BOLETO'}, {type: 'PIX'}],
+//     payment_methods_configs: [
+//       {
+//         type: 'CREDIT_CARD',
+//         config_options: [{option: 'INSTALLMENTS_LIMIT', value: '1'}]
+//       }
+//     ],
+//     redirect_url: 'https://www.google.com'
+//   }
+// };
+
+// axios
+//   .request(options)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
